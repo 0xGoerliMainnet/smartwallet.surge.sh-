2a4d45f16527be8eb7addfd28e834fa053787923
DLM-COINBASE PRO <141368479+0xGoerliMainnet@users.noreply.github.com>
2024-03-22 04:14:22 +0000

    Initialize instawallet

---
+++ packages/react-app/src/App.jsx
@@ -0,0 +1,305 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { useThemeSwitcher } from "react-css-theme-switcher";
+import { Alert, Button, Col, Row } from "antd";
+import "antd/dist/antd.css";
+import React, { useCallback, useEffect, useState } from "react";
+import { HashRouter, Link, Route, Switch } from "react-router-dom";
+import Web3Modal from "web3modal";
+import "./App.css";
+import { Account, Faucet, Header, ThemeSwitch, BigWallet } from "./components";
+import { INFURA_ID, NETWORK, NETWORKS } from "./constants";
+import { useBalance, useContractLoader, useContractReader, useUserSigner } from "./hooks";
+
+const { ethers } = require("ethers");
+/*
+    Welcome to 🏗 scaffold-bch !
+
+    Code: https://github.com/zh/scaffold-eth , Branch: instawallet
+*/
+
+/// 📡 What chain are your contracts deployed to?
+// const targetNetwork = NETWORKS.localhost;
+const targetNetwork = NETWORKS.testnetSmartBCH;
+// const targetNetwork = NETWORKS.mainnetSmartBCH;
+
+// 😬 Sorry for all the console logging
+const DEBUG = false;
+const NETWORKCHECK = true;
+
+const contractName = "InstaWalletToken";
+
+// 🛰 providers
+// 🏠 Your local provider is usually pointed at your local blockchain
+const localProviderUrl = targetNetwork.rpcUrl;
+if (DEBUG) console.log("🏠 Connecting to provider:", localProviderUrl);
+const localProvider = new ethers.providers.StaticJsonRpcProvider(localProviderUrl);
+
+// 🔭 block explorer URL
+const blockExplorer = targetNetwork.blockExplorer;
+
+/*
+  Web3 modal helps us "connect" external wallets:
+*/
+const web3Modal = new Web3Modal({
+  network: "mainnet", // Optional. If using WalletConnect on xDai, change network to "xdai" and add RPC info below for xDai chain.
+  cacheProvider: true, // optional
+  theme: "light", // optional. Change to "dark" for a dark theme.
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        bridge: "https://polygon.bridge.walletconnect.org",
+        infuraId: INFURA_ID,
+        rpc: {
+          1: `https://mainnet.infura.io/v3/${INFURA_ID}`, // mainnet // For more WalletConnect providers: https://docs.walletconnect.org/quick-start/dapps/web3-provider#required
+          42: `https://kovan.infura.io/v3/${INFURA_ID}`,
+          100: "https://dai.poa.network", // xDai
+        },
+      },
+    },
+  },
+});
+
+function App(props) {
+  const [injectedProvider, setInjectedProvider] = useState();
+  const [address, setAddress] = useState();
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if (injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function") {
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  };
+
+  /* 💵 This hook will get the price of ETH from 🦄 Uniswap: */
+  // const price = useExchangePrice(targetNetwork, mainnetProvider);
+  const price = 0;
+
+  /* 🔥 This hook will get the price of Gas from ⛽️ EtherGasStation */
+  const gasPrice = targetNetwork.gasPrice || 1050000000; // SmartBCH minimal fee
+  // if (DEBUG) console.log("⛽️ Gas price:", gasPrice);
+  // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
+  const userSigner = useUserSigner(injectedProvider, localProvider);
+
+  useEffect(() => {
+    async function getAddress() {
+      if (userSigner) {
+        const newAddress = await userSigner.getAddress();
+        setAddress(newAddress);
+      }
+    }
+    getAddress();
+  }, [userSigner]);
+
+  // You can warn the user if you would like them to be on a specific network
+  const localChainId = localProvider && localProvider._network && localProvider._network.chainId;
+  const selectedChainId =
+    userSigner && userSigner.provider && userSigner.provider._network && userSigner.provider._network.chainId;
+
+  // For more hooks, check out 🔗eth-hooks at: https://www.npmjs.com/package/eth-hooks
+
+  // 🏗 scaffold-eth is full of handy hooks like this one to get your balance:
+  const yourLocalBalance = useBalance(localProvider, address);
+
+  // Just plug in different 🛰 providers to get your balance on different chains:
+  // const yourMainnetBalance = useBalance(mainnetProvider, address);
+
+  // Load in your local 📝 contract and read a value from it:
+  const readContracts = useContractLoader(localProvider);
+
+  // If you want to make 🔐 write transactions to your contracts, use the userSigner:
+  const writeContracts = useContractLoader(userSigner, { chainId: localChainId });
+
+  useContractReader(readContracts, contractName, "balance");
+
+  //
+  // 🧫 DEBUG 👨🏻‍🔬
+  //
+  useEffect(() => {
+    if (DEBUG && address && selectedChainId && yourLocalBalance && readContracts && writeContracts) {
+      console.log("_____________________________________ 🏗 scaffold-eth _____________________________________");
+      console.log("🏠 localChainId", localChainId);
+      console.log("👩‍💼 selected address:", address);
+      console.log("🕵🏻‍♂️ selectedChainId:", selectedChainId);
+      console.log("💵 yourLocalBalance", yourLocalBalance ? ethers.utils.formatEther(yourLocalBalance) : "...");
+      console.log("📝 readContracts", readContracts);
+      console.log("🔐 writeContracts", writeContracts);
+    }
+  }, [address, selectedChainId, yourLocalBalance, readContracts, writeContracts]);
+
+  let networkDisplay = "";
+  if (NETWORKCHECK && localChainId && selectedChainId && localChainId !== selectedChainId) {
+    const networkSelected = NETWORK(selectedChainId);
+    const networkLocal = NETWORK(localChainId);
+    if (selectedChainId === 1337 && localChainId === 31337) {
+      networkDisplay = (
+        <div style={{ zIndex: 2, position: "absolute", right: 0, top: 60, padding: 16 }}>
+          <Alert
+            message="⚠️ Wrong Network ID"
+            description={
+              <div>
+                You have <b>chain id 1337</b> for localhost and you need to change it to <b>31337</b> to work with
+                HardHat.
+                <div>(MetaMask -&gt; Settings -&gt; Networks -&gt; Chain ID -&gt; 31337)</div>
+              </div>
+            }
+            type="error"
+            closable={false}
+          />
+        </div>
+      );
+    } else {
+      networkDisplay = (
+        <div style={{ zIndex: 2, position: "absolute", right: 0, top: 60, padding: 16 }}>
+          <Alert
+            message="⚠️ Wrong Network"
+            description={
+              <div>
+                You have <b>{networkSelected && networkSelected.name}</b> selected and you need to be on{" "}
+                <Button
+                  onClick={async () => {
+                    const ethereum = window.ethereum;
+                    const data = [
+                      {
+                        chainId: "0x" + targetNetwork.chainId.toString(16),
+                        chainName: targetNetwork.name,
+                        nativeCurrency: targetNetwork.nativeCurrency,
+                        rpcUrls: [targetNetwork.rpcUrl],
+                        blockExplorerUrls: [targetNetwork.blockExplorer],
+                      },
+                    ];
+                    console.log("data", data);
+
+                    let switchTx;
+                    // https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
+                    try {
+                      switchTx = await ethereum.request({
+                        method: "wallet_switchEthereumChain",
+                        params: [{ chainId: data[0].chainId }],
+                      });
+                    } catch (switchError) {
+                      // not checking specific error code, because maybe we're not using MetaMask
+                      try {
+                        switchTx = await ethereum.request({
+                          method: "wallet_addEthereumChain",
+                          params: data,
+                        });
+                      } catch (addError) {
+                        // handle "add" error
+                      }
+                    }
+
+                    if (switchTx) {
+                      console.log(switchTx);
+                    }
+                  }}
+                >
+                  <b>{networkLocal && networkLocal.name}</b>
+                </Button>
+                .
+              </div>
+            }
+            type="error"
+            closable={false}
+          />
+        </div>
+      );
+    }
+  } else {
+    networkDisplay = (
+      <div style={{ zIndex: -1, position: "absolute", right: 154, top: 28, padding: 16, color: targetNetwork.color }}>
+        {targetNetwork.name}
+      </div>
+    );
+  }
+
+  const loadWeb3Modal = useCallback(async () => {
+    try {
+      const provider = await web3Modal.connect();
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+
+      provider.on("chainChanged", chainId => {
+        console.log(`chain changed to ${chainId}! updating providers`);
+        setInjectedProvider(new ethers.providers.Web3Provider(provider));
+      });
+
+      provider.on("accountsChanged", () => {
+        console.log(`account changed!`);
+        setInjectedProvider(new ethers.providers.Web3Provider(provider));
+      });
+
+      // Subscribe to session disconnection
+      provider.on("disconnect", (code, reason) => {
+        console.log(code, reason);
+        logoutOfWeb3Modal();
+      });
+    } catch (e) {
+      console.log(`Wev3Modal error: ${e}`);
+    }
+  }, [setInjectedProvider]);
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+  const [route, setRoute] = useState();
+  useEffect(() => {
+    setRoute(window.location.pathname);
+  }, [setRoute]);
+
+  const faucetAvailable = localProvider && localProvider.connection && targetNetwork.name.indexOf("local") !== -1;
+  const { currentTheme } = useThemeSwitcher();
+
+  return (
+    <div className="App">
+      {/* ✏️ Edit the header and change the title to your project name */}
+      <Header />
+      {networkDisplay}
+      <HashRouter>
+        <Switch>
+          <Route exact path="/:pk?">
+            <div style={{ width: 480, margin: "auto", marginTop: 32, paddingBottom: 32 }}>
+              <BigWallet signer={userSigner} provider={localProvider} price={price} gasPrice={gasPrice} />
+            </div>
+          </Route>
+        </Switch>
+      </HashRouter>
+
+      <ThemeSwitch />
+
+      {/* 👨💼 Your account is in the top right with a wallet at connect options */}
+      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
+        <Account
+          address={address}
+          localProvider={localProvider}
+          userSigner={userSigner}
+          price={price}
+          showBalance={false}
+          web3Modal={web3Modal}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={logoutOfWeb3Modal}
+          blockExplorer={blockExplorer}
+        />
+      </div>
+
+      {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
+      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
+        <Row align="middle" gutter={[4, 4]}>
+          <Col span={24}>
+            {
+              /*  if the local provider has a signer, let's show the faucet:  */
+              faucetAvailable ? <Faucet signer={userSigner} localProvider={localProvider} price={price} /> : ""
+            }
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/react-app/src/components/TokenBalance.jsx
@@ -0,0 +1,51 @@
+import { useTokenBalance } from "eth-hooks";
+import React, { useState } from "react";
+
+import { utils } from "ethers";
+
+export default function TokenBalance(props) {
+  const [dollarMode, setDollarMode] = useState(true);
+
+  const tokenContract = props.contracts && props.contracts[props.name];
+  const balance = useTokenBalance(tokenContract, props.address, 1777);
+
+  let floatBalance = parseInt(balance); // parseFloat("0.00")
+
+  let usingBalance = balance;
+
+  if (typeof props.balance !== "undefined") {
+    usingBalance = props.balance;
+  }
+
+  if (usingBalance) {
+    /*
+    const etherBalance = utils.formatEther(usingBalance);
+    parseFloat(etherBalance).toFixed(2);
+    floatBalance = parseFloat(etherBalance);
+    */
+    floatBalance = parseInt(usingBalance);
+  }
+
+  // let displayBalance = floatBalance.toFixed(4);
+  let displayBalance = floatBalance;
+
+  if (props.dollarMultiplier && dollarMode) {
+    displayBalance = "$" + (floatBalance * props.dollarMultiplier).toFixed(2);
+  }
+
+  return (
+    <span
+      style={{
+        verticalAlign: "middle",
+        fontSize: 24,
+        padding: 8,
+        cursor: "pointer",
+      }}
+      onClick={() => {
+        setDollarMode(!dollarMode);
+      }}
+    >
+      {displayBalance} {props.img} {props.suffix}
+    </span>
+  );
+}
