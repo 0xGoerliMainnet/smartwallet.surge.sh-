8755bbe0c90f35b8a9f496712887802e9f51d0d0
DLM-COINBASE PRO <141368479+0xGoerliMainnet@users.noreply.github.com>
2024-03-22 04:14:25 +0000

    Initialize spa-wallet

---
+++ packages/react-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { PageHeader } from "antd";
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+  return (
+    <a href="https://github.com/zh/scaffold-eth/tree/spa-wallet" target="_blank" rel="noopener noreferrer">
+      <PageHeader title="SPA Wallet" style={{ cursor: "pointer" }} />
+    </a>
+  );
+}
---
+++ packages/react-app/src/constants.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
+export const INFURA_ID = process.env.REACT_APP_INFURA_KEY;
+
+// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = process.env.REACT_APP_ETHERSCAN_KEY;
+
+// BLOCKNATIVE ID FOR Notify.js:
+export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
+
+export const NETWORKS = {
+  localhost: {
+    name: "localhost",
+    color: "#666666",
+    chainId: 31337,
+    blockExplorer: "",
+    rpcUrl: "http://" + window.location.hostname + ":8545",
+  },
+  testnetSmartBCH: {
+    name: "SmartBCH-T",
+    color: "#7003DD",
+    chainId: 10001,
+    rpcUrl: "https://moeing.tech:9545", // "http://35.220.203.194:8545",
+    blockExplorer: "https://smartscan.cash/",
+    gasPrice: 1050000000,
+  },
+  mainnetSmartBCH: {
+    name: "SmartBCH",
+    color: "#ff8b9e",
+    chainId: 10000,
+    rpcUrl: "https://global.uat.cash", // https://smartbch.greyh.at
+    blockExplorer: "https://smartscan.cash/",
+    gasPrice: 1050000000,
+  },
+};
+
+export const NETWORK = chainId => {
+  for (const n in NETWORKS) {
+    if (NETWORKS[n].chainId === chainId) {
+      return NETWORKS[n];
+    }
+  }
+};
+
+export const FIAT_PRICE = process.env.REACT_APP_FIAT_PRICE === "YES";
